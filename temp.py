3af393b03b188d4531f153d96016ec77bbe0738c
Rostislav Zaliznyak <russ_zaliznyak@intuit.com>
2026-01-16 16:35:54 -0800

    save

--- temp.py
+++ temp.py
@@ -1,21 +1,18 @@
-observed_power = np.mean(
-    np.where(
-        alt_simulations > significance_threshold,
-        1,
-        0,
-    )
-)
-prob_above_effect = np.mean(
-    np.where(
-        alt_simulations
-        > delta_to_control_planned,
-        1,
-        0,
-    )
+prior_mean = 0
+prior_se = 0.0025
+(
+    posterior_mean,
+    posterior_se,
+) = combine_prior_and_likelihood(
+    prior_mean, prior_se, delta, se_delta
 )
 
-pr_over_estimate_statssig = (
-    prob_above_effect / observed_power
+plot_distributions_cdf_only(
+    prior_mean=prior_mean,
+    prior_se=prior_se,
+    like_mean=delta,
+    like_se=se_delta,
+    post_mean=posterior_mean,
+    post_se=posterior_se,
+    metric_name="Conversions",
 )
-
-pr_over_estimate_statssig
